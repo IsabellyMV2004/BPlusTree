10a2367206f185ab46dcfa733aa9871aa8cb4ac8
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-06-14 21:36:23 -0300

    socorro

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -17,19 +17,22 @@ public class Aplicacao
         System.out.println("\n\n# # # # IN-ORDEM DA ARVORE TREE # # #");
         b.in_ordem();
 
-        System.out.println("EXCLUIR VALORES:");
-        for(int i = 50; i> 0; i--){
+        System.out.println("\nEXCLUIR VALORES:");
+        for(int i = 1; i <= 5; i++){
             System.out.printf(i+" ");
-            if(i%20 == 0)
-                System.out.println();
             b.excluir(i);
-            b.exibir();
+            System.out.printf((i+10)+" ");
+            b.excluir(i+10);
+            System.out.printf((i+100)+" ");
+            b.excluir(i+100);
+            System.out.printf((i+200)+" ");
+            b.excluir(i+200);
         }
-        System.out.println("------------------------------");
-      //  b.exibir();
+        System.out.printf("\n\n# # # ARVORE B+TREE DEPOIS DA EXCLUSÃO # # #");
+        b.exibir();
 
 
-        /*System.out.println("\n\n\n-------------------------------------------------------------------------------\n");
+        System.out.println("\n\n\n-------------------------------------------------------------------------------\n");
         System.out.println("N = 5");
         b = new BPlusTree(5);
         System.out.println("\nINSERINDO VALORES:");
@@ -42,16 +45,19 @@ public class Aplicacao
         System.out.println("\n\n# # # ARVORE B+TREE # # #");
         b.exibir();
 
-        /*System.out.println("EXCLUIR VALORES:");
-        for(int i = 50; i > 0; i--){
+        System.out.println("\nEXCLUIR VALORES:");
+        for(int i = 1; i <= 5; i++){
             System.out.printf(i+" ");
-            if(i%20 == 0)
-                System.out.println();
             b.excluir(i);
-            b.exibir();
+            System.out.printf((i+10)+" ");
+            b.excluir(i+10);
+            System.out.printf((i+100)+" ");
+            b.excluir(i+100);
+            System.out.printf((i+200)+" ");
+            b.excluir(i+200);
         }
-        System.out.println("------------------------------");
-        b.exibir();*/
+        System.out.printf("\n\n# # # ARVORE B+TREE DEPOIS DA EXCLUSÃO # # #");
+        b.exibir();
 
 
     }
